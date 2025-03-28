ccb628bc73f9ca741c40b5da9530c7ce61443ce1
Joel Erulu <joelerulu088@gmail.com>
2025-03-28 13:22:46 -0500

    Naming error

---
+++ src/app/services/room.services.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoomServices {
+  private apiUrl = 'https://polar-pair-backend.vercel.app/rooms';
+
+  constructor(private http: HttpClient) {}
+
+  createRoom(username: string, roomName: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/create/${username}/${roomName}`, {});
+  }
+
+  deleteRoom(roomCode: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/delete/${roomCode}`);
+  }
+
+  getRoom(username: string, roomCode: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/room/${username}/${roomCode}`);
+  }
+
+  createParticipant(roomCode: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/participant/create/${roomCode}`, {});
+  }
+
+  deleteParticipant(roomCode: string, name: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/participant/delete/${roomCode}/${name}`);
+  }
+
+  createPairing(roomCode: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/pairings/create/${roomCode}`, {});
+  }
+
+  getPairings(roomCode: string, name: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/pairings/${roomCode}/${name}`);
+  }
+}
