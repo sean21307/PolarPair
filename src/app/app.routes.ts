2e3eaba22085729bda79a60671e926c94b18b91a
Sean Nolan <seannolan21307@gmail.com>
2025-03-28 09:46:05 -0500

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 export const routes: Routes = [
     { path: 'auth', component: AuthComponent },
-    { path: '',   redirectTo: '/auth', pathMatch: 'full' },
+    { path: '', component: LandingComponent },
 ];
